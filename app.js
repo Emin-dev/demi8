cd3ae2c2b9c9174451340ddefe5edf47e66f53ff
Emin <ealdunn99@gmail.com>
2021-04-20 16:40:42 +0000

    final.todo.list

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 
-$("#me").click(function(){
-  $("#me").toggleClass("run");
+
+
+
+
+$("button").click(function () { 
+    let txt = $("input").val();
+    todo(txt);
+    $("input").val('');
 });
+
+function todo(txt) {
+     let h4 = $("<h4></h4>").text(txt)
+    let li = $("<li></li>").append(h4);
+    $("ul").append(li);
+}
--- dist/app.dev.js
+++ dist/app.dev.js
@@ -1,5 +1,13 @@
 "use strict";
 
-$("#me").click(function () {
-  $("#me").toggleClass("run");
+$("button").click(function () {
+  var txt = $("input").val();
+  todo(txt);
+  $("input").val('');
 });
+
+function todo(txt) {
+  var h4 = $("<h4></h4>").text(txt);
+  var li = $("<li></li>").append(h4);
+  $("ul").append(li);
+}
